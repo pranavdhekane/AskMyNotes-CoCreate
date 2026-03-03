ec2ab893655bc020f6320d7b1d8e525fb78ea091
pranavdhekane <pranavdhekane006@gmail.com>
2026-03-03 17:47:14 +0530

    minor fix

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
 module.exports.handler = serverless(app);
